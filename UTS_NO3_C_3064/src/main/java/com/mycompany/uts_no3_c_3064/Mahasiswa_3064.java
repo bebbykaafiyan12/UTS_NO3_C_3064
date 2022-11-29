eeed9f1c86647a40f09e3bc6f6f3f640694375b4
bebbykaafiyan12 <106333416+bebbykaafiyan12@users.noreply.github.com>
2022-11-29 20:42:04 +0700

    Add files via upload

---
+++ UTS_NO3_C_3064/src/main/java/com/mycompany/uts_no3_c_3064/Mahasiswa_3064.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.uts_no3_c_3064;
+
+/**
+ *
+ * @author YUNITA
+ */
+public class Mahasiswa_3064 {
+    protected String nim, nama, jurusan;
+    protected int ipk;
+    
+    public void tampilDataMhs() {
+        System.out.println(" NIM    : " + nim);
+        System.out.println("Nama    " + nama);
+        System.out.println("Jurusan    : " + jurusan);
+        System.out.println("IPK : " + ipk);
+    }
+}
+
